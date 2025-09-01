672cbb995add3609df40271a5ee9e13e282f3845
yongheon Jeong <jung09965555@gmail.com>
2025-09-01 13:25:14 +0900

    add tictactoe

---
+++ Ch03/dynamicAlloc.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n = 0,m = 0,count=0;
+	cout << "행의 개수? : ";
+	cin >> n;
+	cout << "열의 개수? : ";
+	cin >> m;
+
+	int** M = new int* [n];
+	for (int i = 0; i < n; i++) {
+		M[i] = new int[m];
+		for (int j = 0; j < m; j++) {
+			M[i][j] = count++;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << M[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < n; i++) {
+		delete[] M[i];
+	}
+	delete[] M;
+
+}
